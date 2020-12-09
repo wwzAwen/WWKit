2d91e1669e10cc4a81c613e34366749dcff469eb
王文照 <wangwenzhao@didachuxing.com>
2020-12-09 18:23:30 +0800

    移除无用文件

--- 未命名文件夹/Pods/Target Support Files/Pods-WWProjectTests/Pods-WWProjectTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WWProjectTests : NSObject
-@end
-@implementation PodsDummy_Pods_WWProjectTests
-@end
--- 未命名文件夹/Pods/Target Support Files/Pods-WWProjectUITests/Pods-WWProjectUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WWProjectUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WWProjectUITests
-@end
